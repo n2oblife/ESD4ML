c84bc6389ff847b9ba3fb8d9d9a459977002e5c0
n2oblife <zackanit@gmail.com>
2025-01-19 21:25:00 +0100

    unsigned multiplier done

--- lab3_vhdl/multiplier.vhd
+++ lab3_vhdl/multiplier.vhd
@@ -91,7 +91,6 @@ architecture IP_multiplier of multiplier is
 
    constant c_dataWidth    : integer := g_dataWidth;                                      -- data width
    constant c_carry_null   : std_logic := '0';                                            -- initial carry
-   constant c_Cvec         : std_logic_vector(g_dataWidth-1 downto 0) := (other => '0');  -- initial carry
 
 -------------------------------------------------------------------------------------------------
 -- SIGNALS
@@ -136,7 +135,7 @@ begin
    -- generic Carry Lookahead Adder
    -- Process Description : CLA for generic signals and data width
    -- Details : procedure should have a delay of g_dataWidth clock cycle if clocked
-   -- and can handle one bit augmentation at the end
+   -- and can handle one bit augmentation at the end using the carry signal
    -- This procedure doesn't have a data width limitation and can be used for any data width
    procedure p_CLA(
       signal i_A           : in std_logic_vector;
@@ -146,18 +145,16 @@ begin
       signal o_C           : out std_logic;
       constant dataWidth   : integer
    ) is
-      variable s_C : std_logic_vector; -- TODO : check if works with signal if error
+      variable s_C : std_logic_vector(0 to dataWidth-2); -- TODO : check if works with signal if error
    begin
       -- 1st stage
-      p_partial_full_adder(i_A(0), i_B(0), i_C, o_S(0), s_C(s_C'low));
+      p_partial_full_adder(i_A(0), i_B(0), i_C, o_S(0), s_C(0));
       -- stage 2 to before last
-      for i in 1 to dataWidth-1 loop
-         p_partial_full_adder(i_A(i), i_B(i), s_C(i), o_S(i), s_C(i+1));
+      for i in 1 to dataWidth-2 loop
+         p_partial_full_adder(i_A(i), i_B(i), s_C(i), o_S(i), s_C(i));
       end loop;
       -- last stage
-      p_partial_full_adder(i_A(i_A'high), i_B(i_B'high), s_C(s_C'high), o_S(o_S'high-1), o_C);
-      -- final output
-      o_S(o_S'high) <= o_C;
+      p_partial_full_adder(i_A(i_A'high), i_B(i_B'high), s_C(s_C'high), o_S(o_S'high), o_C);
    end procedure p_CLA;
 
 
@@ -199,7 +196,7 @@ begin
    -- Process Description : Multiplier for generic signals and data width
    -- Details : procedure should have a delay of g_dataWidth clock cycle if clocked
    -- and can handle one bit augmentation at the end => o_S must be twice the size of input
-   procedure p_multiplier(
+   procedure p_uns_multiplier(
       signal i_A           : in std_logic_vector;
       signal i_B           : in std_logic_vector;
       signal o_S           : out std_logic_vector;
@@ -212,25 +209,117 @@ begin
       -- 1st stage
       p_bit_word_multiplier(i_A(0), i_B, s_P(0));
       p_bit_word_multiplier(i_A(1), i_B, s_P(1));
+      -- bit padding for correctness
       p_CLA('0' & s_P(0), s_P(1) & '0', c_carry_null, s_acc(0), s_C(0), s_P(0)'length+1);
       
       o_S(0)      <= s_acc(0)(0);
 
       -- stage 2 to before last
-      for i in 2 to dataWidth-1 loop
+      for i in 2 to dataWidth-2 loop
          -- TODO : finish this
+         -- watch for bit shifting when adding
          p_bit_word_multiplier(i_A(i), i_B, s_P(i));
-         p_CLA(s_acc(i-2), s_P(i), c_carry_null, s_acc(i-1), );
+         p_CLA(
+            '0' & s_acc(i-2)(s_acc(i-2)'high downto 1),
+            s_P(i) & '0',
+            s_C(i-2),
+            s_acc(i-1),
+            s_C(i-1),
+            s_P(i)'length+1
+         );
          o_S(i-1) <= s_acc(i-1)(0); 
       end loop;
 
       -- last stage
+      p_bit_word_multiplier(i_A(i_A'high), i_B, s_P(dataWidth-1));
+      p_CLA(
+         '0' & s_acc(s_acc'high)(s_acc(s_acc'high)'high downto 1),
+         s_P(s_P'high) & '0',
+         s_C(s_C'high),
+         s_acc(s_acc'high),
+         s_C(s_C'high),
+         s_P'length+1
+      );
+      o_S(2*dataWidth-1 downto dataWidth-1) <= s_C(s_C'high) & s_acc(s_acc'high);
+   end procedure p_uns_multiplier;
 
-      for i in 0 to g_dataWidth-1 loop
-         -- need some padding when adding
-         p_CLA(s_accZ0, s_P(i+1), c_carry_null, s_accZ1)
+
+   procedure p_sig_multiplier(
+      signal i_A           : in std_logic_vector;
+      signal i_B           : in std_logic_vector;
+      signal o_S           : out std_logic_vector;
+      constant dataWidth   : integer
+   ) is
+      variable s_C   : array(0 to dataWidth-1) of std_logic;
+      variable s_P   : array(0 to dataWidth-1) of std_logic_vector;
+      variable s_acc : array(0 to dataWidth-1) of std_logic_vector;
+   begin
+      -- 1st stage
+      p_bit_word_multiplier(i_A(0), i_B, s_P(0));
+      p_bit_word_multiplier(i_A(1), i_B, s_P(1));
+      -- bit padding for correctness
+      p_CLA('0' & s_P(0), s_P(1) & '0', c_carry_null, s_acc(0), s_C(0), s_P(0)'length+1);
+      
+      o_S(0)      <= s_acc(0)(0);
+
+      -- stage 2 to before last
+      for i in 2 to dataWidth-2 loop
+         -- TODO : finish this
+         -- watch for bit shifting when adding
+         p_bit_word_multiplier(i_A(i), i_B, s_P(i));
+         p_CLA(
+            '0' & s_acc(i-2)(s_acc(i-2)'high downto 1),
+            s_P(i) & '0',
+            s_C(i-2),
+            s_acc(i-1),
+            s_C(i-1),
+            s_P(i)'length+1
+         );
+         o_S(i-1) <= s_acc(i-1)(0); 
       end loop;
-   end procedure p_multiplier;
+
+      -- last stage
+      p_bit_word_multiplier(i_A(i_A'high), i_B, s_P(dataWidth-1));
+      p_CLA(
+         '0' & s_acc(s_acc'high)(s_acc(s_acc'high)'high downto 1),
+         s_P(s_P'high) & '0',
+         s_C(s_C'high),
+         s_acc(s_acc'high),
+         s_C(s_C'high),
+         s_P'length+1
+      );
+      o_S(2*dataWidth-1 downto dataWidth-1) <= s_C(s_C'high) & s_acc(s_acc'high);      
+   end procedure p_sig_multiplier;
+
+
+   -- IP multiplier
+   -- Process Description: IP multiplier for generic signals and data width
+   -- Process is synchronous to FPGA's clock
+   -- Additional details: This process should have a delay of g_dataWidth clock cycle if clocked
+   P_IP_multiplier : process(clk)
+   begin
+      if rising_edge(clk) then
+         
+         -- signed and vectorized
+         if i_S and i_V then
+         end if;
+
+         -- unsigned and vectorized
+         if (not i_S) and i_V then
+         end if;
+
+         -- signed and unvectorized
+         if i_S and (not i_V) then
+            p_multiplier(i_A, i_B, o_Y, c_dataWidth);
+         end if;
+
+         p_uns_multiplier(i_A, i_B, o_Y, c_dataWidth);
+         
+         if reset then
+            o_Y <= (others => '0');
+         end if;
+      end if;
+   end process P_IP_multiplier;
 
 -------------------------------------------------------------------------------------------------
 end architecture IP_multiplier;
